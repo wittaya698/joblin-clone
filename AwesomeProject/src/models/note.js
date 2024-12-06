28e627be3d50f663409be138993cc4bbe67b026e
TarWittaya <wittayathongjeen698@gmail.com>
2024-12-06 19:17:22 +0700

    Only save latlong if saving new note

--- AwesomeProject/src/models/note.js
+++ AwesomeProject/src/models/note.js
@@ -41,35 +41,31 @@ class Note extends BaseModel {
             });
     }
 
-    static byFolderId() {}
+    static updateGeolocation(noteId) {
+        Log.info('Updating lat/long of note ' + noteId);
+
+        let geoData = null;
+        Geolocation.currentPosition()
+            .then(data => {
+                Log.info('Got lat/long');
+                geoData = data;
+                return Note.load(note.id);
+            })
+            .then(note => {
+                if (!note) return; // Has been deleted in the meantime
+                note.longitude = geoData.coords.longitude;
+                note.latitude = geoData.coords.latitude;
+                note.altitude = geoData.coords.altitude;
+                Note.save(note, { updateLatLong: false });
+            })
+            .catch(error => {
+                Log.info('Cannot get location:', error);
+            });
+    }
 
     static save(o, options = null) {
-        if (!options) options = {};
-        if (!('updateLatLong' in options)) options.updateLatLong = true;
-
         return super.save(o, options).then(note => {
             this.dispatch(actions.notes_update_one({ note: note }));
-
-            if (options.updateLatLong && !note.latitude && !note.longitude) {
-                Log.info('Updating lat/long of note...');
-                let geoData = null;
-                Geolocation.currentPosition()
-                    .then(data => {
-                        Log.info('Got lat/long');
-                        geoData = data;
-                        return Note.load(note.id);
-                    })
-                    .then(note => {
-                        if (!note) return; // Has been deleted in the meantime
-                        note.longitude = geoData.coords.longitude;
-                        note.latitude = geoData.coords.latitude;
-                        note.altitude = geoData.coords.altitude;
-                        Note.save(note, { updateLatLong: false });
-                    })
-                    .catch(error => {
-                        Log.info('Cannot get location:', error);
-                    });
-            }
         });
     }
 }
