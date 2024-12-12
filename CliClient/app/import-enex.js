a9bfe274758b96343157a1f2077a8579b9f33b55
TarWittaya <wittayathongjeen698@gmail.com>
2024-12-12 12:42:28 +0700

    Read and parse XMl

--- CliClient/app/import-enex.js
+++ CliClient/app/import-enex.js
@@ -7,19 +7,45 @@ const __dirname = dirname(__filename);
 
 appModulePath.addPath(__dirname);
 
-import { WebApi } from './src/web-api.js';
+import Promise from 'promise';
+import fs from 'fs';
+import xml2js from 'xml2js';
 
-// setTimeout(() => {
-// 	console.info('ici');
-// }, 1000);
+function parseXml(xml) {
+    return new Promise((resolve, reject) => {
+        xml2js.parseString(xml, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
 
-let api = new WebApi('http://localhost:8000');
-console.log('API: ', api);
+function readFile(path, options = null) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, options, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
 
-api.post('sessions', null, {
-    email: 'wittayathongjeen698@gmail.com',
-    password: '0906198331',
-    client_id: 'C1C1C1C1C1C1C1'
-}).then(session => {
-    console.info(session);
-});
+readFile('/Users/macbookair/Workspace/sample-enex.xml', 'utf8')
+    .then(content => {
+        return parseXml(content);
+    })
+    .then(doc => {
+        let notes = doc['en-export']['note'];
+        for (let i = 0; i < notes.length; i++) {
+            let note = notes[i];
+            console.info(note);
+        }
+    })
+    .catch(error => {
+        console.error('Error reading XML file', error);
+    });
