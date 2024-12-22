b4a02647fdfd9b2eefeca753657022125bfc0dc1
TarWittaya <wittayathongjeen698@gmail.com>
2024-12-22 20:34:16 +0700

    Test sync

--- CliClient/run_test.sh
+++ CliClient/run_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 CLIENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-rm -f "$CLIENT_DIR/spec-build/src"
-ln -s "$CLIENT_DIR/build/src" "$CLIENT_DIR/spec-build"
+rm -f "$CLIENT_DIR/tests-build/src"
+mkdir -p "$CLIENT_DIR/tests-build"
+ln -s "$CLIENT_DIR/build/src" "$CLIENT_DIR/tests-build"
 
-npm run build && NODE_PATH="$CLIENT_DIR/spec-build/" npm run test spec-build/synchronizer.js
+npm run build && NODE_PATH="$CLIENT_DIR/tests-build/" npm test tests-build/synchronizer.js
---
+++ ReactNativeClient/src/time-utils.js
@@ -0,0 +1,7 @@
+let time = {
+    unix() {
+        return Math.round(new Date().getTime() / 1000);
+    }
+};
+
+export { time };
