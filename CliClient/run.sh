58048f0195bdb602971cec7e2c0dba1f4d2a086d
TarWittaya <wittayathongjeen698@gmail.com>
2025-01-06 13:45:43 +0700

    UI to set config

--- CliClient/run.sh
+++ CliClient/run.sh
@@ -3,4 +3,4 @@ set -e
 CLIENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 bash $CLIENT_DIR/build.sh
-NODE_PATH="$CLIENT_DIR/build/" node build/main.js --profile ~/Temp/TestNotes --sync-target onedrive
+NODE_PATH="$CLIENT_DIR/build/" node build/main.js --profile ~/Temp/TestNotes --sync-target local
