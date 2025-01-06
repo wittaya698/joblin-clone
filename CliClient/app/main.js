51c8593700a104e12a00d92e6345321a94dc97cf
TarWittaya <wittayathongjeen698@gmail.com>
2025-01-06 18:21:07 +0700

    Handle command line args

--- CliClient/app/main.js
+++ CliClient/app/main.js
@@ -42,17 +42,17 @@ let logger = new Logger();
 let dbLogger = new Logger();
 let syncLogger = new Logger();
 
-commands.push({
-    usage: 'root',
-    options: [['-p, --profile <filePath>', 'Sets the profile path directory.']],
-    action: function (args, end) {
-        if (args.profile) {
-            initArgs.profileDir = args.profile;
-        }
+// commands.push({
+//     usage: 'root',
+//     options: [['-p, --profile <filePath>', 'Sets the profile path directory.']],
+//     action: function (args, end) {
+//         if (args.profile) {
+//             initArgs.profileDir = args.profile;
+//         }
 
-        end();
-    }
-});
+//         end();
+//     }
+// });
 
 commands.push({
     usage: 'config [name] [value]',
@@ -661,16 +661,46 @@ function cmdPromptConfirm(commandInstance, message) {
 // route them to the "root" command.
 function handleStartArgs(argv) {
     return new Promise((resolve, reject) => {
-        if (argv && argv.length >= 3 && argv[2][0] == '-') {
-            const startParams = vorpal.parse(argv, { use: 'minimist' });
-            const cmd = commandByName('root');
-            cmd.action(startParams, () => {
-                resolve();
-            });
-        } else {
-            // TODO
-            resolve();
+        while (true) {
+            argv.splice(0, 2);
+            if (argv[0] == '--profile') {
+                argv.splice(0, 1);
+                if (!argv.length) throw new Error(_('Profile path is missing'));
+                initArgs.profileDir = argv[0];
+                argv.splice(0, 1);
+            } else if (argv[0][0] === '-') {
+                throw new Error(_('Unknown flag: "%s"', argv[0]));
+            }
+
+            if (!argv.length || argv[0][0] != '-') {
+                resolve(argv);
+            }
+
+            return;
+
+            // if (argv && argv.length >= 3 && argv[2][0] == '-') {
+            // 	const startParams = vorpal.parse(argv, { use: 'minimist' });
+            // 	const cmd = commandByName('root');
+            // 	cmd.action(startParams, (newArgs) => {
+            // 		console.info(newArgs);
+            // 		resolve();
+            // 	});
+            // } else {
+            // 	console.info(argv);
+            // 	resolve();
+            // }
         }
+        // if (argv && argv.length >= 3 && argv[2][0] == '-') {
+        // 	const startParams = vorpal.parse(argv, { use: 'minimist' });
+        // 	const cmd = commandByName('root');
+        // 	cmd.action(startParams, (newArgs) => {
+        // 		console.info(newArgs);
+        // 		resolve();
+        // 	});
+        // } else {
+        // 	console.info(argv);
+        // 	resolve();
+        // }
     });
 }
 
