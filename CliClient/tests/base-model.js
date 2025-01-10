08dde73ab92fd53f7376f9808aa5f6f478853bea
TarWittaya <wittayathongjeen698@gmail.com>
2025-01-10 18:15:47 +0700

    Better handling of db errors

--- CliClient/tests/base-model.js
+++ CliClient/tests/base-model.js
@@ -11,7 +11,6 @@ import {
     clearDatabase,
     switchClient
 } from 'test-utils.js';
-import { createFoldersAndNotes } from 'test-data.js';
 import { Folder } from 'lib/models/folder.js';
 import { Note } from 'lib/models/note.js';
 import { Setting } from 'lib/models/setting.js';
