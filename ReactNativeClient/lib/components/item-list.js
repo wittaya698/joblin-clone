727e0196250c65e2bd202926091da1a343537421
TarWittaya <wittayathongjeen698@gmail.com>
2025-01-25 09:06:26 +0700

    Fixed display of todos

--- ReactNativeClient/lib/components/item-list.js
+++ ReactNativeClient/lib/components/item-list.js
@@ -42,7 +42,11 @@ class ItemListComponent extends Component {
         for (let i = 0; i < notes.length; i++) {
             const note = notes[i];
             if (note.is_todo) {
-                if (todoFilter == 'recent' && note.updated_time < notRecentTime)
+                if (
+                    todoFilter == 'recent' &&
+                    note.updated_time < notRecentTime &&
+                    !!note.todo_completed
+                )
                     continue;
                 if (todoFilter == 'nonCompleted' && !!note.todo_completed)
                     continue;
