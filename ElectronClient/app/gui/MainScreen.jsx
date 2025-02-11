7ecf97b7b7545ddd568bb18ebc29bd52a9fc174e
TarWittaya <wittayathongjeen698@gmail.com>
2025-02-12 01:02:12 +0700

    Update locale

--- ElectronClient/app/gui/MainScreen.jsx
+++ ElectronClient/app/gui/MainScreen.jsx
@@ -168,7 +168,7 @@ class MainScreenComponent extends React.Component {
         } else if (command.name === 'search') {
             this.setState({
                 promptOptions: {
-                    label: _('Seach:'),
+                    label: _('Search:'),
                     onClose: async answer => {
                         if (answer !== null) {
                             const searchId = uuid.create();
@@ -278,7 +278,7 @@ class MainScreenComponent extends React.Component {
         });
 
         headerButtons.push({
-            title: _('Seach'),
+            title: _('Search'),
             iconName: 'fa-search',
             onClick: () => {
                 this.doCommand({ name: 'search' });
